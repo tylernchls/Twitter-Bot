044b69237a76141f46f7c7c26c91738ee406ab7d
Tyler Nichols <tylernichols@Tylers-MacBook-Pro-2.local>
2017-05-23 12:44:36 -1000

    changed variable names

---
+++ bot.js
@@ -0,0 +1,66 @@
+const twit = require('twit');
+const CONFIG = require('./bot_config');
+const fs = require('fs');
+const Twitter = new twit(CONFIG);
+const stream = Twitter.stream('user');
+
+const sendTweet = message => {
+
+  let tweet = {
+      status: message
+  }
+  const catchError = (err, data, response) => {
+    if (err) {
+      console.log('Something went wrong ' + err);
+    } else {
+      console.log('Success!!! Your Tweet Has Been Posted :)');
+    }
+  }
+
+  Twitter.post('statuses/update', tweet, catchError());
+}
+
+const tweetEvent = tweetMsg => {
+
+  const Twitter_Handle = tweetMsg.in_reply_to_screen_name;
+  let msg_data = JSON.stringify(tweetMsg, null, 2)
+  fs.writeFile('tweets.json', msg_data);
+  let msg_text = tweetMsg.text;
+  let sender = tweetMsg.user.screen_name;
+
+  if(Twitter_Handle) {
+    let respondTweet = ('@' + sender + " Thanks for the tweet");
+    sendTweet(respondTweet);
+  }
+}
+
+const newFollower = eventMessage => {
+
+  let name = eventMessage.source.name;
+  let user_screenName = eventMessage.source.screen_name;
+
+  Twitter.post('direct_messages/new', {
+      screen_name : user_screenName,
+      text: "Whats up Brah"
+    },
+    function(err, data, response) {
+      if(err) {
+        console.log('Something went wrong ' + err);
+      } else {
+        console.log('Your direct message has been sent to ' + user_screenName);
+      }
+    });
+}
+
+stream.on('follow', newFollower);
+stream.on('tweet', tweetEvent);
+
+
+
+
+
+
+
+
+
+
---
+++ example_config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  consumer_key: "Enter your key here",
+  consumer_secret: "Enter your key here",
+  access_token: "Enter your access_token here",
+  access_token_secret: "Enter secret token here",
+  timeout_ms: 60*1000 // optional timeout
+}
